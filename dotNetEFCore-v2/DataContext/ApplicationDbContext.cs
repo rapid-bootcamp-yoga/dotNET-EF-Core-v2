1170b55f3511701b391d6c49df05fb94635a9b19
yogajulianp <yogajprasutiyo@gmail.com>
2023-01-10 09:47:47 +0700

    db migration MySQL

---
+++ dotNetEFCore-v2/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotNetEFCore_v2.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        
+        }
+        public DbSet<CategoryEntity> CategoryEntities => Set<CategoryEntity>();
+
+    }
+}
---
+++ dotNetEFCore-v2/DataContext/CategoryEntity.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotNetEFCore_v2.DataContext
+{
+    [Table("tbl_category")]
+    public class CategoryEntity
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("category_code")]
+        public String CategoryCode { get; set; }
+
+        [Column("category_name")]
+        public String CategoryName { get; set; }
+
+        [Column("description")]
+        public String Description { get; set; }
+
+        public CategoryEntity()
+        {
+
+        }
+       
+       
+    }
+}
